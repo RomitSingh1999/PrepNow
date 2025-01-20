b07dfe81d2888c9bafd8bdacf3db6f37b3f141cb
RomitSingh1999 <romitsingh19@gmail.com>
2025-01-20 16:57:11 +0530

    unwanted push 102

--- src/main/java/org/preparation/Main.java
+++ src/main/java/org/preparation/Main.java
@@ -1,9 +1,18 @@
 package org.preparation;
 
+import org.preparation.designPatterns.creational.factory.OSFactory;
+import org.preparation.designPatterns.creational.factory.Os;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Trying to learn Design Patterns");
+        OSFactory factory= new OSFactory();
+        Os newOs=factory.getOsInstance("laptopFlexible");
+        if(newOs==null) {
+            System.out.println("No Os detected!");
+            return;
+        }
+        System.out.println(newOs.spec());
     }
 }
